4c6053900e7df51eadbc389c105a5b3b2847adcb
phan.nguyen <phan.nguyen@mail.utoronto.ca>
2026-04-13 23:54:45 -0700

    changing collage

--- Eval/yolo_pred.py
+++ Eval/yolo_pred.py
@@ -11,7 +11,7 @@ DATA_ROOT = Path(__file__).resolve().parent.parent
 IMAGE_DIR = DATA_ROOT / "compare" / "pred_images"
 PROJECT_DIR = str((DATA_ROOT / 'yolo_train_out').resolve())
 PROJECT_DIR = Path(PROJECT_DIR) 
-RUN_NAME    = 'yolo_pt_auto_label'
+RUN_NAME    = 'yolo_pt_manual_label'
 
 best_weights = PROJECT_DIR / RUN_NAME / 'weights' / 'best.pt'
 
@@ -25,7 +25,7 @@ results = yolo_eval.predict(
     conf=0.35,
     save=True,
     show_labels=False,   
-    show_conf=False,     
+    show_conf=True,     
     project=PROJECT_DIR,
     name=RUN_NAME + "_pred",
     exist_ok=True,
